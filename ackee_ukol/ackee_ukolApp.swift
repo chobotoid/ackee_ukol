f7672b2a814bbec3a338ff7e540bc74a4590b460
Chobotoid <honzagutwirth@seznam.cz>
2023-06-30 19:09:00 +0200

    Initial Commit

---
+++ ackee_ukol/ackee_ukolApp.swift
@@ -0,0 +1,17 @@
+//
+//  ackee_ukolApp.swift
+//  ackee_ukol
+//
+//  Created by Jan Gutwirth on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ackee_ukolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
